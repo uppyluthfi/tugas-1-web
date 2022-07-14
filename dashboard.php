e7a1b99eff3ef876ea39bb9f2fc3bca8e2e0e44a
uppyluthfi <108688105+uppyluthfi@users.noreply.github.com>
2022-07-14 17:57:46 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,32 @@
+<h1>AWAL MUALA TERBENTUKNYA UIN ALAUDDIN MAKASSAAR</h1>
+<pre>
+SEJARAH PERKEMBANGAN
+Sejarah perkembangan Universitas Islam Negeri Alauddin  Makassar, yang dulu Institut Agama Islam Negeri (IAIN) Alauddin Makassar melalui beberapa fase yaitu:
+
+1. Fase tahun 1962 s.d 1965
+
+Pada mulanya IAIN Alauddin Makassar yang kini menjadin UIN Alauddin Makassar berstatus Fakultas Cabang dari IAIN Sunan Kalijaga Yogyakarta, atas desakan Rakyat dan Pemerintah Daerah Sulawesi Selatan serta atas persetujuan Rektor IAIN Sunan Kalijaga Yogyakarta, Menteri Agama Republik Indonesia mengeluarkan Keputusan Nomor 75 tanggal 17 Oktober 1962 tentang penegerian Fakultas Syari'ah UMI menjadi Fakultas Syari'ah IAIN Sunan Kalijaga Yogyakarta Cabang Makassar pada tanggal 10 Nopember 1962. Kemudian menyusul penegerian Fakultas Tarbiyah UMI menjadi Fakultas Tarbiyah IAIN Sunan Kalijaga Yogyakarta Cabang Makassar pada tanggal 11 Nopember 1964 dengan Keputusan Menteri Agama Nomor 91 tanggal 7 Nopember 1964. Kemudian Menyusul pendirian Fakultas Ushuluddin IAIN Sunan Kalijaga Yogyakarta cabang Makassar tanggal 28 Oktober 1965 dengan Keputusan Menteri Agama Nomor 77  tanggal 28 Oktober 1965.
+
+2. Fase tahun 1965 s.d 2005
+
+Dengan mempertimbangkan dukungan dan hasrat yang besar  dari rakyat dan Pemerintah Daerah Sulawesi Selatan terhadap pendidikan dan pengajaran agama Islam tingkat Universitas, serta landasan hukum Peraturan Presiden Nomor 27 tahun 1963 yang antara lain menyatakan bahwa dengan sekurang-kurangnya tiga jenis fakultas IAIN dapat digabung menjadi satu institut tersendiri sedang tiga fakultas dimaksud telah ada di Makassar, yakni Fakultas Syari'ah, Fakultas Tarbiyah dan Fakultas Ushuluddin, maka mulai tanggal 10 Nopember 1965 berstatus mandiri dengan nama Institut Agama Islam Negeri Al-Jami'ah al-Islamiyah al-Hukumiyah di Makassar dengan Keputusan Menteri Agama Nomor 79 tanggal 28 Oktober 1965.
+
+Penamaan IAIN di Makassar dengan “Alauddin” diambil dari nama raja Kerajaan Gowa yang  pertama memuluk Islam dan memiliki latar belakang sejarah pengembangan Islam di masa silam, di samping mengandung harapan peningkatan kejayaan Islam di masa mendatang di Sulawesi Selatan pada khususnya dan Indonesia bahagian Timur pada umumnya. Sultan Alauddin adalah raja Gowa XIV tahun 1593-1639, (kakek/datok) dari Sultan Hasanuddin Raja Gowa XVI, dengan nama lengkap I Mangnga'rangi Daeng Manrabbia Sultan Alauddin, yang setelah wafatnya digelari juga dengan Tumenanga ri Gaukanna (yang mangkat dalam kebesaran kekuasaannya), demikian menurut satu versi, dan menurut versi lainnya gelar setelah wafatnya itu adalah Tumenanga ri Agamana (yang wafat dalam agamanya). Gelar Sultan Alauddin diberikan kepada Raja Gowa XIV ini, karena dialah Raja Gowa yang pertama kali menerima agama Islam sebagai agama kerajaan. Ide pemberian nama “ Alauddin ” kepada IAIN yang berpusat di Makassar tersebut, mula pertama dicetuskan oleh para pendiri IAIN “ Alauddin” , di antaranya adalah Andi Pangeran Daeng Rani, (cucu/turunan) Sultan Alauddin, yang juga mantan Gubernur Sulawesi Selatan, dan Ahmad Makkarausu Amansyah Daeng Ilau, ahli sejarah Makassar.
+
+Pada Fase ini, IAIN (kini UIN) Alauddin yang semula hanya memiliki tiga (3) buah Fakultas, berkembang menjadi lima (5)  buah Fakultas ditandai dengan berdirinya Fakuktas Adab berdasarkan Keputusan Menteri Agama RI No. 148 Tahun 1967 Tanggal 23 Nopember 1967, disusul Fakultas Dakwah dengan Keputusan Menteri Agama RI No.253 Tahun 1971 dimana Fakultas ini berkedudukan di Bulukumba (   153 km arah selatan kota Makassar), yang selanjutnya dengan Keputusan Presiden  RI No.9 Tahun 1987 Fakultas Dakwah dialihkan ke Makassar, kemudian disusul pendirian Program Pascasarjana (PPs) dengan Keputusan Dirjen Binbaga Islam Dep. Agama No. 31/E/1990 tanggal 7 Juni 1990 berstatus kelas jauh dari PPs IAIN Syarif Hidayatullah Jakarta yang kemudian dengan Keputusan Menteri Agama RI No. 403 Tahun 1993 PPs IAIN Alauddin Makassar menjadi PPs yang mandiri.
+
+3. Fase Tahun 2005 s.d sekarang
+
+Untuk merespon tuntutan perkembangan ilmu pengetahuan dan perubahan mendasar atas lahirnya  Undang-Undang Sistem Pendidikan Nasional No.2 tahun 1989 di mana jenjang pendidikan pada Departemen Pendidikan Nasional R.I dan Departemen Agama R.I, telah disamakan kedudukannya khususnya jenjang pendidikan menegah, serta untuk menampung lulusan jenjang pendidikan menengah di bawah naungan Departemen Pendidikan Nasional R.I dan Departemen Agama R.I, diperlukan perubahan status Kelembagaan dari Institut menjadi Universitas, maka atas prakarsa pimpinan IAIN Alauddin periode 2002-2006 dan atas dukungan civitas Akademika dan  Senat IAIN Alauddin serta Gubernur  Sulawesi Selatan, maka diusulkanlah konversi IAIN Alauddin Makassar menjadi UIN Alauddin Makassar kepada Presiden R.I melalui Menteri Agama R.I dan Menteri Pnedidikan Nasional R.I. Mulai 10 Oktober 2005 Status Kelembagaan Institut Agama Islam Negeri (IAIN) Alauddin Makassar berubah menjadi (UIN) Universitas Islam Negeri Alauddinn  Alauddin Makassar berdasarkan Peraturan Presiden (Perpres) Republik Indonesia No 57 tahun 2005 tanggal 10 Oktober 2005 yang ditandai dengan peresmian penandatanganan prasasti oleh Presiden RI Bapak DR H Susilo Bambang Yudhoyono pada tanggal 4 Desember 2005 di Makassar.
+
+Dalam perubahan status kelembagaan dari Institut ke Universitas , UIN Alauddin Makasar mengalami perkembangan dari lima (5) buah Fakutas menjadi 7 (tujuh) buah Fakultas dan 1 (satu) buah Program Pascasarjana (PPs) berdasarkan Peraturan Menteri Agama RI Nomor 5 tahun 2006 tanggal 16 Maret 2006, yaitu:
+
+Fakuktas Syariah dan Hukum (FSH)
+Fakuktas Tarbiyah dan Keguruan (FTK)
+Fakultas Ushuluddin dan Filsafat (FUF)
+Fakultas Adab dan Humaniora (FAH)
+Fakultas Dakwah dan Komunikasi (FDK)
+Fakultas Sains dan Teknologi (FST)
+Fakultas Kedokteran dan Ilmu Kesehatan (FIK)
+Prgoram Pascasarjana(PPs)
+</pre>
---
+++ template.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Blank Page</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100&display=swap" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="./assets/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="./assets/dist/css/adminlte.min.css">
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  
+</head>
+<body class="hold-transition sidebar-mini">
+
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="info">
+          <h1>Muh Luthfi Mubarak</h1>
+        </div>
+      </div>
+      
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1><?= $content['title'] ?> </h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Blank Page</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <?php   include "dashboard.php"   ?>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer> -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="./assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="./assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="./assets/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="./assets/dist/js/demo.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  
+</body>
+</html>
